0ce10df32c8658c8a3bc1ab1fd10ecd382665a33
Ben-Pyth <86666370+Ben-Pyth@users.noreply.github.com>
2024-11-24 21:36:40 +0700

    update

--- BaronCoffee/Models/ViewModel/OrderHistory.cs
+++ BaronCoffee/Models/ViewModel/OrderHistory.cs
@@ -8,11 +8,10 @@ namespace BaronCoffee.Models.ViewModel
     public class OrderHistory
     {
         public int OrderID { get; set; }
-        public int UserID { get; set; }
-        public int ProductID { get; set; }
-        public int Quantity { get; set; }
-        public string Status { get; set; }  // Trạng thái đơn hàng (Đang chờ, Hoàn thành)
-        public decimal TotalAmount { get; set; }
         public DateTime OrderDate { get; set; }
+        public string Status { get; set; }
+        public decimal TotalAmount { get; set; }
+        public List<CartItem> Products { get; set; }  // Danh sách sản phẩm
     }
+
 }

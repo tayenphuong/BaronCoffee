0a64a4b34e7f3f6e08cfe153a37c4cf8eed453a4
thuytrieu045 <123728064+thuytrieu045@users.noreply.github.com>
2024-11-24 21:30:01 +0700

    Add files via upload

---
+++ BaronCoffee/Controllers/ProductDetailController.cs
@@ -0,0 +1,32 @@
+﻿using BaronCoffee.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaronCoffee.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        private MyStoreEntities1 db = new MyStoreEntities1();
+        // GET: ProductDetail
+        public ActionResult Index()
+        {
+            var products = db.Products.ToList(); // Lấy tất cả sản phẩm
+            return View(products); // Truyền danh sách sản phẩm tới View
+        }
+        public ActionResult Details(int id)
+        {
+            // Tìm sản phẩm theo ID
+            var product = db.Products.FirstOrDefault(p => p.ProductId == id);
+            if (product == null)
+            {
+                return HttpNotFound(); // Trả về lỗi nếu không tìm thấy
+            }
+
+            return View(product); // Truyền sản phẩm tới View
+        }
+    }
+}

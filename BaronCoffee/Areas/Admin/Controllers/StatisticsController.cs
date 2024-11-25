41a69b9a7172cbc1d58e06e1bbe9451072f8d1b7
thuytrieu045 <123728064+thuytrieu045@users.noreply.github.com>
2024-11-25 21:24:00 +0700

    Add files via upload

---
+++ BaronCoffee/Areas/Admin/Controllers/StatisticsController.cs
@@ -0,0 +1,36 @@
+﻿using BaronCoffee.Models;// Đường dẫn tới model và DbContext
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaronCoffee.Areas.Admin.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        // GET: Admin/Statistics
+
+        private MyStoreEntities db = new MyStoreEntities();
+
+        // Action: Thống kê đơn hàng của khách hàng
+        public ActionResult CustomerStatistics(int customerId)
+        {
+            // Lấy thông tin khách hàng
+            var customer = db.Customers.Find(customerId);
+            if (customer == null)
+            {
+                return HttpNotFound("Không tìm thấy khách hàng.");
+            }
+
+            // Đếm số lượng đơn hàng của khách hàng
+            int orderCount = db.Orders.Count(o => o.CustomerID == customerId);
+
+            // Truyền dữ liệu vào view
+            ViewBag.CustomerName = customer.CustomerName;
+            ViewBag.OrderCount = orderCount;
+
+            return View();
+        }
+    }
+}

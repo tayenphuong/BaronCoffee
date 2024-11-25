3660f075e18a8ac029e8d46a7fd750c7defd84b9
HaoNgo1404 <haodong14042005@gmail.com>
2024-11-25 13:08:23 +0700

    sửa code

--- BaronCoffee/Models/ViewModel/ChangePasswordVM.cs
+++ BaronCoffee/Models/ViewModel/ChangePasswordVM.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace LapTrinhWeb.Models.ViewModel
+namespace BaronCoffee.Models.ViewModel
 {
     public class ChangePasswordVM
     {
--- BaronCoffee/Models/ViewModel/RegisterVM.cs
+++ BaronCoffee/Models/ViewModel/RegisterVM.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace LapTrinhWeb.Models.ViewModel
+namespace BaronCoffee.Models.ViewModel
 {
     public class RegisterVM
     {
